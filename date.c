3c51c903920ab1482d983f81a6ad9287b47598c5
miah <miah.li@wesion.com>
2020-09-03 10:42:00 +0800

    add date.c
    Signed-off-by: miah <miah.li@wesion.com>

--- date.c
+++ date.c
@@ -9,17 +9,17 @@ int main()
 {
         int fd,temp;
         char buff[100];
-        while(1)
+        while (1)
         {
                 fd = open("/sys/bus/w1/devices/28-0119395ebf91/w1_slave", O_RDONLY);
-                char *string1=buff;
-                char *string2="t=";
-                read(fd,buff,sizeof(buff));
-                printf("%s\n",buff);
-                char *a=strstr(string1, string2);
-                printf("a:%s\n",a);
+                char *string1 = buff;
+                char *string2 = "t=";
+                read(fd, buff, sizeof(buff) );
+                printf("%s\n", buff);
+                char *a = strstr(string1, string2);
+                printf("a:%s\n", a);
                 temp = atoi(a+2);                                                               
-                printf("temp:%d.%dC\n",temp/1000,temp%1000/100);
+                printf("temp:%d.%dC\n", temp/1000, temp%1000/100);
                 sleep(1);
                 close(fd);
         }
